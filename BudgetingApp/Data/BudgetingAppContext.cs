790570c0b77858869a3313ce9de7fb6929d87a75
FairuzZarif <155786897+FairuzZarif@users.noreply.github.com>
2025-06-05 20:17:48 -0600

    Add project files.

---
+++ BudgetingApp/Data/BudgetingAppContext.cs
@@ -0,0 +1,13 @@
+﻿using BudgetingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetingApp.Data
+{
+    public class BudgetingAppContext : DbContext
+    {
+        public BudgetingAppContext(DbContextOptions<BudgetingAppContext> options) : base(options) { } // Constructor that accepts DbContextOptions for configuration
+
+        // instance of DbSet for managing expenses in the database
+        DbSet<Expense> Expenses { get; set; } // DbSet for managing expenses in the database
+    }
+}
---
+++ BudgetingApp/Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetingApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; } // Unique identifier for the expense
+        [Required]
+        public string Description { get; set; } = null!; // Description of the expense, cannot be null
+        [Required]
+        public double Amount { get; set; } // Amount of the expense, cannot be negative
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount needs to be higher than 0")] // data attribute for validation and error handling
+        public DateTime Date { get; set; } = DateTime.Now; // Date of the expense, cannot be in the future
+        public string Category { get; set; } // e.g., "Food", "Transport", etc.
+    }
+}
