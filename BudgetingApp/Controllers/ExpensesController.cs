e1bdde2dc373adf6be93ca1b39a1f5904081b6e6
FairuzZarif <155786897+FairuzZarif@users.noreply.github.com>
2025-06-06 00:47:07 -0600

    Created the controller for the expenses.

---
+++ BudgetingApp/Controllers/ExpensesController.cs
@@ -0,0 +1,18 @@
+﻿using BudgetingApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetingApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly BudgetingAppContext _context;
+        public ExpensesController(BudgetingAppContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
